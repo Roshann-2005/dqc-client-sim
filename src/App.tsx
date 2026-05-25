83bd1da7b6129af52616ee2f6225fb2df4ef3f52
Riley O'Shannessy <riley@Rileys-Mac-mini.local>
2026-05-25 20:31:18 +1000

    just make app serve simulator

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Landing from './pages/Landing'
+
 import Simulator from './pages/Simulator'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/simulator" element={<Simulator />} />
+        <Route path="/" element={<Simulator />} />
       </Routes>
     </BrowserRouter>
   )
